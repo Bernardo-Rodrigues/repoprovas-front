f8ddc5b18c8640c66363c6fdd487cfd11bd68888
Bernardo Rodrigues <bernardo.rodrigues06@unifei.edu.br>
2022-04-21 13:36:07 -0300

    feat: add user login

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route, Navigate } from "react-router-dom"
+import { SignIn } from "../pages/SignIn"
 import { SignUp } from "../pages/SignUp"
 
 export default function AppRoutes() {
     return(
         <Routes>
-            <Route path="/" element={ <SignUp/> } />
+            <Route path="/sign-up" element={ <SignUp/> } />
+            <Route path="/sign-in" element={ <SignIn/> } />
 
-            <Route path="*" element={ <Navigate to="/"/> } />
+            <Route path="*" element={ <Navigate to="/sign-up"/> } />
         </Routes>
     )
 }
